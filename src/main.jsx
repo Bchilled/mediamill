f2309da06cf715fe6fd67ca221f8a661896a84a8
MediaMill Build <mediamill@build.ai>
2026-03-05 15:13:34 +0000

    fix: main.jsx mangled by string replace

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'
+import App from './App';
 import{Sounds}from './utils/sounds';
 import './index.css';
 import{I18nProvider}from './i18n';
 import{installGlobalErrorHandlers}from './utils/errorTracker';
 
 installGlobalErrorHandlers();
+try{setTimeout(()=>Sounds.startup(),400);}catch(e){}
 
-ReactDOM.try{setTimeout(()=>Sounds.startup(),400);}catch(e){}
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <I18nProvider><App/></I18nProvider>
 );
